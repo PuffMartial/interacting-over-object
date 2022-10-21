c76abbda547a3f7803ed8251ab3d96052b892989
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2022-10-21 10:42:12 +0200

    Add files via upload
    try it out if u want

---
+++ iterating_over_objects/type.js
@@ -0,0 +1,20 @@
+// const expenses = {
+//     rent: 10000,
+//     food: 2000,
+//     internet: 500,
+// }
+// // let total = 0;
+// // for(const key in expenses){
+// //     total += expenses[key]
+    
+// // }
+// // console.log(total)
+// console.log(Object.keys(expenses), Object.values(expenses))
+
+// const total = Object.values(expenses).reduce((acc, expenses) => acc += expenses, 0)
+// console.log(total)
+// const keys = Object.keys(expenses);
+// const values = Object.values(expenses);
+// for(let i=0; i<keys.length; i++){
+//     console.log(keys[i], values[i])
+// }
